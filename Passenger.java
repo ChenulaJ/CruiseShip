254d93530c46579befa2d246d8698b84c448baa8
ChenulaJ <102528711+ChenulaJ@users.noreply.github.com>
2022-04-20 22:53:50 +0530

    Add files via upload

---
+++ Passenger.java
@@ -0,0 +1,26 @@
+package cruiseShipClassesSolution;
+
+public class Passenger {
+    public static final String EMPTY_CABIN = "EMPTY";
+
+    private String firstName = EMPTY_CABIN;
+    private String surname = EMPTY_CABIN ;
+    private int expenses = 200;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName.toUpperCase();
+    }
+    public void setSurname (String surname) {
+        this.surname = surname.toUpperCase();
+    }
+
+    public String getFristName() {
+        return firstName;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public int getExpenses() {
+        return expenses;
+    }
+}
