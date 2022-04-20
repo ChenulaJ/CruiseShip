4f9475983fd47e086fa7b4f1b964f8e9479a6c82
ChenulaJ <102528711+ChenulaJ@users.noreply.github.com>
2022-04-20 22:53:20 +0530

    Add files via upload

---
+++ Cabin.java
@@ -0,0 +1,7 @@
+package cruiseShipClassesSolution;
+
+public class Cabin  extends Passenger{
+    public static final int numOfPassengers = 3;
+
+    Passenger[] passenger = new Passenger [numOfPassengers];
+}
